9b11867e35d850259f34ef9fb2c6ae9749adac6e
LawalSamsudeenO <shamslaw564@gmail.com>
2025-12-22 03:45:15 +0100

    setup project structure

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+def get_requirements_from_file(filename):
+    with open(filename, 'r') as file:
+        requirements = file.readlines()
+    # remove empty lines and comments
+    requirements = [req.strip() for req in requirements if req.strip() and not req.startswith('#')]
+    requirements = [req.replace('-e .', '') for req in requirements if req.strip() and not req.startswith('#')]
+    return requirements
+
+setup(
+    name='boston-housing',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=get_requirements_from_file('requirements.txt'),
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='A project for Boston Housing data analysis',
+    url='',
+)
